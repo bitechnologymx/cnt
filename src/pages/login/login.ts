8c4f37cc21fe79ef12fa0c61980d761a107a2f52
aD <bitechnology.mx@gmail.com>
2018-06-29 11:41:41 -0500

    login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -10,6 +10,9 @@ import { HomePage } from '../../pages/home/home';
 })
 export class LoginPage {
 
+  public usuarioImage = "assets/images/foto_login_b.jpg";
+  public usuario:string = "supe";
+
   constructor(public navCtrl: NavController) {
 
   }
@@ -17,6 +20,33 @@ export class LoginPage {
   public dashboard(){
     //this.navCtrl.push(HomePage);
     this.navCtrl.setRoot(Dashboard);
+  }
+
+  public goToDashboard(){
+    var usuarioLogin = this.usuario;
+    if (usuarioLogin=="super" || usuarioLogin=="user"){
+      //$("#video-background").hide();
+      //$("#divVideo").css('background-image', 'url("assets/images/others/img-29.jpg")');
+      if (usuarioLogin=="super"){
+        this.navCtrl.setRoot(Dashboard);
+      } else if (usuarioLogin=="user"){
+        this.navCtrl.setRoot(Dashboard);
+      }
+      return false;
+    }
+  }
+
+  public changeUser(ev){
+    var usuarioInput = ev.target.value;
+    usuarioInput = usuarioInput.toLowerCase();
+    console.log(usuarioInput);
+    if (usuarioInput=="super"){
+      this.usuarioImage = "assets/images/foto_login.jpg";
+    } else if (usuarioInput=="user"){
+      this.usuarioImage = "assets/images/foto_login2.jpg";
+    } else {
+      this.usuarioImage = "assets/images/foto_login_b.jpg";
+    }
   }
 
 }
